0de0ee3b7d56bbfed970f2d6f89afc8eda1c37f6
hc <hheennrryy@gmail.com>
2012-07-29 22:41:58 -0400

    comment out cache code, not needed

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var fs = require('fs')
 var file_partials = {
   top: '/top.hjs-template', bottom: '/bottom.hjs-template' };
 
-var cache = {}
-  , basePath = path.dirname()
+var basePath = path.dirname()
+//  , cache = {}
   , partialsCompiled = {};
 
 function ProcessPartials(partialFiles, partialsIndex, file, options, fn) {
@@ -24,35 +24,38 @@ function ProcessPartials(partialFiles, partialsIndex, file, options, fn) {
       exports.done(file, options, fn);
       return;
     }
-
+/* cache code
     cachedPartial = exports.getCache(options, basePath + partialFile);
     if (cachedPartial) {
       console.log('cache > ' + partialFile);
       partialsCompiled[partialName] = cachedPartial
       at_partial += 1;
       pp.next();
-    } else {
+    } else { */
       exports.compile(basePath + partialFile, options, function(fileCompiled) {
         partialsCompiled[partialName] = fileCompiled;
         at_partial += 1;
         pp.next();
       });
-    }
+    //} cache code
   }
 }
 
+/* cache code
 exports.getCache = function(options, file) {
   if (options.cache && cache[file]) {
     return cache[file]; } else { return false; }
 }
-
 exports.storeCache = function(options, file, fileCompiled) {
   if (options.cache) cache[file] = fileCompiled;
 }
+*/
 
 exports.compile = function(file, options, callback) {
   fs.readFile(file, 'utf8', function(err, data) {
     if (err) throw new Error('couldn\'t read file: ' + file);
+    var fileParse = hoganjs.parse(hoganjs.scan(data), data);
+    console.log(fileParse);
     var fileCompiled = hoganjs.compile(data);
     exports.storeCache(options, file, fileCompiled);
     callback(fileCompiled);
