9fee726016b1c3be1d737388060209aab6d13d15
hc <hheennrryy@gmail.com>
2012-07-31 20:26:47 -0400

    code clean up

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 var fs = require('fs')
-  , hoganjs = require('hogan.js')
-  , path = require('path');
+  , hoganjs = require('hogan.js');
 
-//  , cache = {}
-var partialsCompiled = {};
-
-var initPartials = {};
+var partialsCompiled = {}
+  , initPartials = {};
 
 function ProcessPartials(partials, q, file, options, fn) {
   var pp = this
@@ -14,41 +11,20 @@ function ProcessPartials(partials, q, file, options, fn) {
   this.next = function() {
     // check if done
     if (!q[at_partial] || !partials[q[at_partial]]) {
-      console.log(file);
       exports.done(file, options, fn);
       return;
     }
 
     var partialName = q[at_partial]
       , partialFile = partials[q[at_partial]];
-      //, cachedPartial;
 
-/* cache code
-    cachedPartial = exports.getCache(options, basePath + partialFile);
-    if (cachedPartial) {
-      console.log('cache > ' + partialFile);
-      partialsCompiled[partialName] = cachedPartial
-      at_partial += 1;
-      pp.next();
-    } else { */
       exports.compile(partialFile, options, true, function(fileCompiled) {
         partialsCompiled[partialName] = fileCompiled;
         at_partial += 1;
         pp.next();
       });
-    //} cache code
   }
 }
-
-/* cache code
-exports.getCache = function(options, file) {
-  if (options.cache && cache[file]) {
-    return cache[file]; } else { return false; }
-}
-exports.storeCache = function(options, file, fileCompiled) {
-  if (options.cache) cache[file] = fileCompiled;
-}
-*/
 
 exports.compile = function(partial, options, fixpath, callback) {
   var file = options.settings.views;
@@ -57,14 +33,13 @@ exports.compile = function(partial, options, fixpath, callback) {
 
   fs.readFile(file, 'utf8', function(err, data) {
     if (err) {
+      //console.log(partial);
       // not a file, compile as a string or function
       var compiled = hoganjs.compile(partial);
       callback(compiled);
     } else {
       //var fileParse = hoganjs.parse(hoganjs.scan(data), data);
-      //console.log(fileParse);
       var compiled = hoganjs.compile(data);
-      //exports.storeCache(options, file, fileCompiled);
       callback(compiled);
     }
   });
@@ -74,7 +49,6 @@ exports.compile = function(partial, options, fixpath, callback) {
   after all partials compile then render the original file
 */
 exports.done = function(file, options, fn) {
-  // TODO check cache before compiling
   exports.compile(file, options, false, function(fileCompiled) {
     fn(null, fileCompiled.render(options, partialsCompiled));
   });
